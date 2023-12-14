1f16901349aaf4eb3693476a8a74c02b4a635a41
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-12-13 22:22:05 -0300

    create library consumer application

---
+++ package-consumer/src/App.tsx
@@ -0,0 +1,11 @@
+import { TextField } from "@pedrohlucena/my-package";
+import "./App.css";
+
+console.log("BLABLALBLABLA");
+console.log(TextField);
+
+function App() {
+  return <TextField label="Hello World" />;
+}
+
+export default App;
