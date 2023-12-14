e73bef9099f1b5e904fbae7a9a24403141c49989
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-12-14 20:19:03 -0300

    add css support for the library

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -2,10 +2,12 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json";
-
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+
+import postcss from "rollup-plugin-postcss";
+
+import packageJson from "./package.json";
 
 export default [
   {
@@ -24,10 +26,11 @@ export default [
     ],
     plugins: [
       peerDepsExternal(),
-
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+
+      postcss(),
 
       terser(),
     ],
@@ -36,5 +39,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
