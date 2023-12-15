53825ffa4316a165b5c935dd24746ae34328e40a
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-12-14 21:47:33 -0300

    config vite for lib packaging

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import packageJson from "./package.json";
@@ -9,7 +10,7 @@ const getPackageName = () => {
 
 const getPackageNameCamelCase = () => {
   try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
   } catch (err) {
     throw new Error("Name property in package.json is missing.");
   }
@@ -30,10 +31,17 @@ module.exports = defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: getPackageNameCamelCase(),
       formats,
-      fileName: (format) => fileName[format],
+      fileName: format => fileName[format],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
     },
   },
-  test: {
-
-  }
+  test: {},
+  plugins: [react()],
 });
